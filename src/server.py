f0b0838faeb62957579ca4457fd62e895207dc2c
Charles Ponti <cj@ponti.io>
2024-09-25 16:50:59 -0700

    feat: add validation error handlers and openapi

--- src/server.py
+++ src/server.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.exceptions import RequestValidationError, ResponseValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer
 
 from src.routers.chat_router import chat_router
@@ -29,7 +32,43 @@ app.include_router(audio_router, prefix="/media")
 app.include_router(UserIntentRouter)
 
 
+@app.exception_handler(ResponseValidationError)
+async def response_validation_exception_handler(request, exc):
+    content = {
+        "detail": exc.errors(),
+    }
+    print(content)
+    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=content)
+
+
+@app.exception_handler(RequestValidationError)
+async def request_validation_exception_handler(request, exc):
+    content = {
+        "detail": exc.errors(),
+    }
+    print(content)
+    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content=content)
+
+
 # Root
 @app.get("/info")
 async def root():
     return {"message": "🐍🐍🐍"}
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Snakesss API",
+        version="0.0.1",
+        summary="Snakesss API",
+        description="Snakesss API",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {"url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"}
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
