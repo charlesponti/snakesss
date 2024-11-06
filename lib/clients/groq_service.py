e55a278c7835583c1a64bfc38a85a779964af6cd
Charles Ponti <cj@ponti.io>
2024-11-05 16:28:58 -0800

    chore(*): remove unused or unnec code

--- lib/clients/groq_service.py
+++
@@ -1,7 +0,0 @@
-import os
-
-import groq
-
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY", None)
-
-groq_client = groq.Groq()
--- src/routers/media/audio_router.py
+++ src/routers/media/audio_router.py
@@ -2,8 +2,6 @@ import io
 
 from fastapi import APIRouter, UploadFile
 
-from lib.ai_models import audio_models
-from lib.clients.groq_service import groq_client
 from lib.clients.openai import openai_client
 
 from src.routers.media.vision_router import vision_router
@@ -15,30 +13,13 @@ audio_router.include_router(vision_router, prefix="/vision")
 @audio_router.post("/audio/transcribe")
 def transcribe_audio(
     audio_file: UploadFile,
-    model: str = "openai",
 ):
-    """
-    Transcribes audio using either OpenAI's whisper or Groq's Whisper API.
-    NOTE : OPenAI's transcription is faster as Groq uses whisper large v3 model which is not required at the moment
-    """
+    """Transcribes audio using OpenAI's Whisper API."""
+
     file = audio_file.file.read()
     buffer = io.BytesIO(file)
     buffer.name = "audio.m4a"
 
-    if model == "groq":
-        transcription = groq_client.audio.transcriptions.create(
-            file=buffer,
-            model=audio_models[model],
-            prompt="",
-            response_format="json",
-            language="en",
-            temperature=0.0,
-        )
-
-    elif model == "openai":
-        transcription = openai_client.audio.transcriptions.create(model=audio_models[model], file=buffer)
-
-    else:
-        return {"error": "Choose an appropriate transcription model"}
+    transcription = openai_client.audio.transcriptions.create(model="whisper-1", file=buffer)
 
     return {"result": transcription.text}
